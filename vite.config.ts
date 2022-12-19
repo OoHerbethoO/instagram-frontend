7dec6890a6c2c56e4e9b52e842c962a0a5b841ea
safdar-azeem <safdarazeem00@gmail.com>
2022-12-20 01:17:31 +0500

    config codegen

---
+++ vite.config.ts
@@ -0,0 +1,43 @@
+import { fileURLToPath, URL } from 'node:url'
+import codegen from 'vite-plugin-graphql-codegen'
+import { defineConfig, loadEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
+// https://vitejs.dev/config/
+export default ({ mode }: any) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+  return defineConfig({
+    plugins: [
+      vue(),
+      vueJsx(),
+      codegen({
+        config: {
+          schema: process.env.VITE_APP_API_URL,
+          documents: 'src/**/*.ts',
+          config: {
+            preResolveTypes: true,
+          },
+          generates: {
+            'src/types/graphql.types.ts': {
+              plugins: ['typescript', 'typescript-operations', 'typescript-vue-apollo'],
+              config: {
+                typesPrefix: 'I',
+                skipTypename: true,
+                operationResultSuffix: 'Result',
+                flattenGeneratedTypesIncludeFragments: true,
+                dedupeOperationSuffix: true,
+                vueCompositionApiImportFrom: 'vue',
+              },
+            },
+          },
+        },
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+  })
+}
