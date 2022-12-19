28ff50f1c95c41b405ba4bd95000f3ce3a0460e5
safdar-azeem <safdarazeem00@gmail.com>
2022-12-20 03:12:20 +0500

    Add Avatar component

---
+++ src/components/reusable/Avatar.vue
@@ -0,0 +1,51 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Avatar',
+  props: {
+    src: {
+      type: String,
+      required: true,
+      default: '',
+    },
+    text: {
+      type: String,
+      default: '',
+    },
+    size: {
+      type: String as () => 'sm' | 'md' | 'default' | 'lg',
+      default: 'default',
+    },
+    className: {
+      type: String,
+      default: '',
+    },
+    textClassName: {
+      type: String,
+      default: '',
+    },
+  },
+})
+</script>
+
+<template>
+  <div class="flex gap-x-4 items-center">
+    <img
+      :src="src"
+      class="avatar"
+      :class="{
+        [`${size}`]: size,
+        [`${className}`]: className,
+      }" />
+    <span
+      v-if="text"
+      class="avatar-text"
+      :class="{
+        [`${size}`]: size,
+        [`${textClassName}`]: textClassName,
+      }"
+      >{{ text }}</span
+    >
+  </div>
+</template>
