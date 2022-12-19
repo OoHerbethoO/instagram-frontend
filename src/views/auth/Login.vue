82023ae5e3b5b70f01f7233869cb5472230b976d
safdar-azeem <safdarazeem00@gmail.com>
2022-12-20 01:21:18 +0500

    Add Login page

---
+++ src/views/auth/Login.vue
@@ -0,0 +1,96 @@
+<script lang="ts">
+import Button from '@/components/reusable/Button.vue'
+import Input from '@/components/reusable/Input.vue'
+import useForm from '@/hooks/useForm'
+import { loginJson } from '@/json/login.json'
+import { useLoginMutation } from '@/types/graphql.types'
+import Cookies from 'js-cookie'
+import { defineComponent } from 'vue'
+import { useRouter } from 'vue-router'
+import { useToast } from 'vue-toastification'
+
+export default defineComponent({
+  name: 'Login',
+  components: {
+    Input,
+    Button,
+  },
+  setup() {
+    const router = useRouter()
+    const toast = useToast()
+    const { mutate, onDone, onError, loading } = useLoginMutation({})
+    const { state, handleChange, doValidation } = useForm(loginJson)
+
+    const handleSubmit = (e: Event) => {
+      e.preventDefault()
+      if (doValidation()) {
+        mutate({
+          email: state.email.value,
+          password: state.password.value,
+        })
+      }
+    }
+
+    onDone((result) => {
+      Cookies.set('token', result.data?.login?.token as string)
+      toast.success('User logged in successfully')
+      router.push('/')
+    })
+
+    onError((error) => toast.error(error.message))
+
+    return {
+      state,
+      loading,
+      loginJson,
+      handleChange,
+      handleSubmit,
+    }
+  },
+})
+</script>
+
+<template>
+  <section class="grid lg:grid-cols-2 place-items-center min-h-screen">
+    <aside class="w-full sm:min-w-[541px] sm:w-auto px-10 grid gap-y-8">
+      <header>
+        <h1>Login into account</h1>
+        <p class="mt-1 text-gray-700">Use your credentials to access your account.</p>
+      </header>
+      <form
+        class="grid gap-y-5"
+        @submit="handleSubmit">
+        <Input
+          v-for="item in loginJson"
+          :key="item.name"
+          :label="item.label"
+          :placeholder="item.placeholder"
+          :variant="item.variant"
+          :name="item.name"
+          :value="state[item.name].value"
+          :error="state[item.name].error"
+          @onChange="handleChange"
+          :type="item.type" />
+        <Button
+          text="Login"
+          type="submit"
+          :disabled="loading"
+          :isLoading="loading"
+          variant="neobrutalism" />
+      </form>
+      <footer class="-text-fs-1 text-center">
+        <span>Don’t have an account? </span>
+        <router-link
+          to="/register"
+          class="font-bold ml-1"
+          >Register</router-link
+        >
+      </footer>
+    </aside>
+    <figure class="bg-[#EAE8E2] h-full w-full hidden lg:grid place-items-center">
+      <img
+        src="/register-illustration.svg"
+        alt="" />
+    </figure>
+  </section>
+</template>
