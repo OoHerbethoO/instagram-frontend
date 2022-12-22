5b898d38939ddc1d862c3a415a5a49bdffb57a49
safdar-azeem <safdarazeem00@gmail.com>
2022-12-22 15:05:42 +0500

    Add PostList component

---
+++ src/components/post/PostList.vue
@@ -0,0 +1,41 @@
+<script lang="ts">
+import type { IPost } from '@/types/graphql.types'
+import { defineComponent } from 'vue'
+import PostCard from './PostCard.vue'
+import PostCardSkeleton from '../skeletons/PostCardSkeleton.vue'
+
+export default defineComponent({
+  name: 'PostList',
+  components: { PostCard, PostCardSkeleton },
+  props: {
+    posts: {
+      type: Array as () => Array<IPost>,
+      required: true,
+    },
+    loading: {
+      type: Boolean,
+      required: true,
+    },
+  },
+})
+</script>
+
+<template>
+  <div>
+    <div
+      v-if="loading"
+      class="card-galery">
+      <PostCardSkeleton
+        v-for="i in 4"
+        :key="i" />
+    </div>
+    <div
+      v-else
+      class="card-galery">
+      <PostCard
+        v-for="post in posts"
+        :key="post._id"
+        :post="post" />
+    </div>
+  </div>
+</template>
