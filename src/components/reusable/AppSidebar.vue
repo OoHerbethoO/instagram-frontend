f094adf3215c147fc84471dd0fad14a22027beff
safdar-azeem <safdarazeem00@gmail.com>
2022-12-22 15:02:05 +0500

    Add AppSidebarSkeleton on loading

--- src/components/reusable/AppSidebar.vue
+++ src/components/reusable/AppSidebar.vue
@@ -8,13 +8,14 @@ import { useMeQuery } from '@/types/graphql.types'
 import Cookies from '@/utils/cookies'
 import { useRouter } from 'vue-router'
 import { AppRoutes } from '@/constants/routes.constant'
+import AppSidebarSkeleton from '../skeletons/AppSidebarSkeleton.vue'
 
 export default defineComponent({
   name: 'AppSidebar',
-  components: { Avatar, NavLink, Button },
+  components: { Avatar, NavLink, Button, AppSidebarSkeleton },
   setup() {
     const router = useRouter()
-    const { result } = useMeQuery()
+    const { result, loading } = useMeQuery()
 
     const handleLougout = () => {
       Cookies.removeToken()
@@ -24,6 +25,7 @@ export default defineComponent({
     return {
       navigationJson,
       result,
+      loading,
       handleLougout,
     }
   },
@@ -31,13 +33,32 @@ export default defineComponent({
 </script>
 
 <template>
-  <aside class="w-full flex flex-col justify-between">
+  <AppSidebarSkeleton v-if="!result && loading" />
+
+  <aside
+    class="w-full flex flex-col justify-between faideIn"
+    v-else>
     <header class="w-full">
       <header class="bg-gray-200 p-2 py-3 w-full rounded h-max">
         <Avatar
           :src="result?.me?.avatar || ''"
+          className="bg-gray-300"
           :text="result?.me?.name || ''" />
       </header>
+      <section class="flex justify-between text-center gap-x-3 mt-6">
+        <div>
+          <p>{{ result?.me?.posts?.length }}</p>
+          <p class="-text-fs-1 text-gray-500 font-medium">Posts</p>
+        </div>
+        <div>
+          <p>{{ result?.me?.followers?.length }}</p>
+          <p class="-text-fs-1 text-gray-500 font-medium">Followers</p>
+        </div>
+        <div>
+          <p>{{ result?.me?.following?.length }}</p>
+          <p class="-text-fs-1 text-gray-500 font-medium">Following</p>
+        </div>
+      </section>
       <nav class="mt-10">
         <NavLink
           v-for="item in navigationJson"
