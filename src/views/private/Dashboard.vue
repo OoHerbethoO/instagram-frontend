e5fe456abd249151a8ca2fa8f0ec984e13285873
safdar-azeem <safdarazeem00@gmail.com>
2022-12-22 15:04:51 +0500

    render PostList

--- src/views/private/Dashboard.vue
+++ src/views/private/Dashboard.vue
@@ -1,3 +1,24 @@
+<script lang="ts">
+import { defineComponent, watch } from 'vue'
+import PostList from '@/components/post/PostList.vue'
+import { useGetPostsQuery } from '@/types/graphql.types'
+export default defineComponent({
+  name: 'Dashboard',
+  components: { PostList },
+  setup() {
+    const { result, loading, error } = useGetPostsQuery()
+    watch(result, () => {
+      console.log(result.value?.getPosts)
+    })
+    return { result, loading, error }
+  },
+})
+</script>
+
 <template>
-  <div>dashboard</div>
+  <div>
+    <PostList
+      :posts="result?.getPosts"
+      :loading="loading" />
+  </div>
 </template>
