74a833edbcd0d049af031df54d0b2b2fcde1884b
safdar-azeem <safdarazeem00@gmail.com>
2022-12-22 15:02:51 +0500

    Add Post card Skeleton

---
+++ src/components/skeletons/PostCardSkeleton.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PostCardSkeleton',
+})
+</script>
+
+<template>
+  <article class="post-card">
+    <header class="card-header">
+      <div class="flex items-center gap-3">
+        <div class="avatar skeleton"></div>
+        <h1 class="h-6 w-32 rounded skeleton"></h1>
+      </div>
+      <div class="avatar md skeleton"></div>
+    </header>
+    <div>
+      <h1 class="h-4 w-full rounded skeleton mb-2"></h1>
+      <h1 class="h-4 w-full rounded skeleton"></h1>
+    </div>
+    <div class="card-image skeleton"></div>
+    <footer class="card-footer">
+      <div class="flex items-center gap-5">
+        <div class="avatar skeleton md"></div>
+        <div class="avatar skeleton md"></div>
+      </div>
+      <div class="avatar skeleton md"></div>
+    </footer>
+  </article>
+</template>
