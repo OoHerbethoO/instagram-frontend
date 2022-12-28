3e3e25b46c2446da2d86b94e12acd00227a2aa29
safdar-azeem <safdarazeem00@gmail.com>
2022-12-28 19:04:47 +0500

    Add Dropdown component

---
+++ src/components/reusable/Dropdown.vue
@@ -0,0 +1,41 @@
+<script lang="ts">
+import type { OnClickOutsideHandler } from '@vueuse/core'
+import { onClickOutside } from '@vueuse/core'
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'Dropdown',
+  setup() {
+    const dropdownRef = ref(null)
+    const isDropdownOpen = ref(false)
+
+    const dropdownHandler: OnClickOutsideHandler = (event) => (isDropdownOpen.value = false)
+    onClickOutside(dropdownRef, (event) => (isDropdownOpen.value = false))
+
+    return {
+      dropdownRef,
+      dropdownHandler,
+      isDropdownOpen,
+    }
+  },
+})
+</script>
+
+<template>
+  <div class="dropdown">
+    <div class="dropdown-trigger">
+      <span @click.stop="isDropdownOpen = !isDropdownOpen">
+        <slot name="trigger"></slot>
+      </span>
+    </div>
+    <div
+      class="dropdown-menu"
+      ref="dropdownRef"
+      v-if="isDropdownOpen"
+      v-on-click-outside.bubble="dropdownHandler">
+      <div class="dropdown-content">
+        <slot name="menu"></slot>
+      </div>
+    </div>
+  </div>
+</template>
