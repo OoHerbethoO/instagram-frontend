e20e4e34b98ece11c3ae00343247130c0a3078fd
safdar-azeem <safdarazeem00@gmail.com>
2022-12-28 14:14:18 +0500

    Add Form type

--- src/types/form.type.ts
+++ src/types/form.type.ts
@@ -2,8 +2,9 @@ export interface Form {
   name: string
   placeholder: string
   label?: string
-  type?: 'text' | 'email' | 'password' | 'number' | 'date'
+  type?: 'text' | 'email' | 'password' | 'number' | 'date' | 'textarea'
   value: string
   variant?: 'primary' | 'neobrutalism'
   validation?: (value: string) => string
+  cols?: string
 }
