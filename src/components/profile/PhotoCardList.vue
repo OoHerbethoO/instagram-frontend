538048e493ee3a9a829c928d9d9854eeae6a6c20
safdar-azeem <safdarazeem00@gmail.com>
2022-12-28 14:05:33 +0500

    Add PhotoCardList component

---
+++ src/components/profile/PhotoCardList.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+import type { IPost } from '@/types/graphql.types'
+import { defineComponent } from 'vue'
+import PhotoCard from './PhotoCard.vue'
+
+export default defineComponent({
+  name: 'PhotoCardList',
+  components: { PhotoCard },
+  props: {
+    posts: {
+      type: Object as () => IPost[],
+      required: true,
+    },
+    loading: {
+      type: Boolean,
+      required: true,
+    },
+  },
+})
+</script>
+
+<template>
+  <main class="grid sm:grid-cols-2 gap-10 flex-1">
+    <PhotoCard
+      v-for="post in posts"
+      :key="post?._id"
+      :post="post" />
+  </main>
+</template>
