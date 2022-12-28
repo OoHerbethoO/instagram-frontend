86a59a1f16a90bf146268fef89160f3e1f23cdea
safdar-azeem <safdarazeem00@gmail.com>
2022-12-28 14:11:18 +0500

    Add CreatePost mutation

--- src/graphql/mutations/createPost.mutation.ts
+++ src/graphql/mutations/createPost.mutation.ts
@@ -3,7 +3,7 @@ import PostFragment from '../fragments/post.fragment'
 
 export const CreatePostMutation = gql`
   ${PostFragment}
-  mutation CreatePost($content: String!, $photo: String, $isPublic: Boolean) {
+  mutation CreatePost($content: String, $photo: String, $isPublic: Boolean) {
     createPost(content: $content, photo: $photo, isPublic: $isPublic) {
       ...post
     }
