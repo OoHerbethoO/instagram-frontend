290da3b4eacbd139848adb37157af537fa3d27af
safdar-azeem <safdarazeem00@gmail.com>
2022-12-28 14:13:52 +0500

    Add UpdateProfile form Json

---
+++ src/json/updateProfile.json.ts
@@ -0,0 +1,56 @@
+import type { Form } from '../types/form.type'
+
+export const updateProfileJson: Form[] = [
+  {
+    name: 'name',
+    placeholder: 'Add your name...',
+    label: 'Name',
+    type: 'text',
+    value: '',
+    cols: 'md:col-span-2',
+    validation(value: string) {
+      return value.length > 0 ? '' : 'Name is required'
+    },
+  },
+  {
+    name: 'bio',
+    placeholder: 'Add a short bio...',
+    label: 'Bio',
+    type: 'textarea',
+    value: '',
+    cols: 'md:col-span-2',
+  },
+  {
+    name: 'location',
+    placeholder: 'Add a location...',
+    label: 'Location',
+    type: 'text',
+    value: '',
+  },
+  {
+    name: 'dateOfBirth',
+    placeholder: 'When you were born',
+    label: 'When you were born',
+    type: 'date',
+    value: '',
+  },
+  {
+    name: 'profession',
+    placeholder: 'What do you do?',
+    label: 'Profession',
+    type: 'text',
+    value: '',
+  },
+  {
+    name: 'website',
+    placeholder: 'Add your website link',
+    label: 'Website',
+    type: 'text',
+    value: '',
+    validation(value: string) {
+      if (!value) return ''
+      const url = new URL(value)
+      return url.protocol === 'http:' || url.protocol === 'https:' ? '' : 'Invalid URL'
+    },
+  },
+]
