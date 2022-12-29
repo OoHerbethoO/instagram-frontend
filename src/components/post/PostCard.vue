19075c9f76c019c52b041f09e8ca0bed063443f8
safdar-azeem <safdarazeem00@gmail.com>
2022-12-30 02:09:32 +0500

    display and format date using moment

--- src/components/post/PostCard.vue
+++ src/components/post/PostCard.vue
@@ -1,10 +1,12 @@
 <script lang="ts">
+import { AppRoutes } from '@/constants/routes.constant'
 import type { IPost, IUser } from '@/types/graphql.types'
 import { defineComponent, reactive, toRefs, watch } from 'vue'
 import Avatar from '../reusable/Avatar.vue'
 import Button from '../reusable/Button.vue'
 import PostActionMenu from './PostActionMenu.vue'
 import PostCardFooter from './PostCardFooter.vue'
+import moment from 'moment'
 
 export default defineComponent({
   name: 'PostCard',
@@ -18,6 +20,7 @@ export default defineComponent({
       type: Object as () => IUser | null,
       required: true,
     },
+    height: String,
   },
   setup(props) {
     const state = reactive({
@@ -38,36 +41,53 @@ export default defineComponent({
       { immediate: true }
     )
 
-    return { ...toRefs(state) }
+    return { ...toRefs(state), AppRoutes, moment }
+  },
+
+  computed: {
+    time() {
+      console.log(this.post.createdAt)
+      return moment(this.post.createdAt).fromNow()
+    },
   },
 })
 </script>
 
 <template>
-  <article class="post-card">
-    <header class="card-header">
-      <Avatar
-        :src="post?.user?.avatar || ''"
-        :text="post?.user.name"
-        size="md"
-        class="card-avatar" />
-      <PostActionMenu
-        :postId="post._id"
-        :isCurrentUserPost="isCurrentUserPost" />
-    </header>
-    <p>{{ post.content }}</p>
-    <figure>
-      <img
-        :src="post?.photo || ''"
-        class="card-image"
-        alt="" />
-    </figure>
-    <PostCardFooter
-      :isLiked="isLiked"
-      :totalLikes="post?.likes.length"
-      :isBookmark="isBookmark"
-      :postId="post?._id"
-      :me="me"
-      :totalComments="post?.comments.length" />
-  </article>
+  <Transition name="card">
+    <article class="post-card">
+      <header class="card-header mb-1">
+        <router-link :to="`${AppRoutes.PROFILE}/${post?.user?._id}`">
+          <Avatar
+            :src="post?.user?.avatar || ''"
+            :text="post?.user.name"
+            :time="time"
+            size="md"
+            class="card-avatar" />
+        </router-link>
+        <PostActionMenu
+          :postId="post._id"
+          :isCurrentUserPost="isCurrentUserPost" />
+      </header>
+      <p
+        class="text-gray-700"
+        v-if="post.content">
+        {{ post.content }}
+      </p>
+      <figure>
+        <img
+          :src="post?.photo || ''"
+          class="card-image"
+          @click="$emit('openModal')"
+          alt="" />
+      </figure>
+      <PostCardFooter
+        :isLiked="isLiked"
+        :totalLikes="post?.likes.length"
+        :isBookmark="isBookmark"
+        :postId="post?._id"
+        :me="me"
+        :totalComments="post?.comments.length" />
+    </article>
+  </Transition>
 </template>
