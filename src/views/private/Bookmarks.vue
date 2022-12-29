1a6b1f77c7f87d1ace6c4c6ea0db6f639d2bd4af
safdar-azeem <safdarazeem00@gmail.com>
2022-12-30 02:14:37 +0500

    Add Bookmarks page

---
+++ src/views/private/Bookmarks.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import { useGetBookmarkedPostsQuery } from '@/types/graphql.types'
+import { defineComponent } from 'vue'
+import PostList from '@/components/post/PostList.vue'
+
+export default defineComponent({
+  name: 'Bookmarks',
+  components: { PostList },
+  setup() {
+    const { result, loading: bookmarksLoading } = useGetBookmarkedPostsQuery()
+    return {
+      result,
+      bookmarksLoading,
+    }
+  },
+})
+</script>
+
+<template>
+  <header class="h5 font-medium">My Bookmarked ({{ result?.getBookmarkedPosts.length }})</header>
+  <section class="mt-8">
+    <PostList
+      :posts="result?.getBookmarkedPosts"
+      :loading="bookmarksLoading" />
+  </section>
+</template>
