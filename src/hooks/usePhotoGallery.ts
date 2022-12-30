b0eaacc9d07c339e9e665b9b2e8225940048bcd0
safdar-azeem <safdarazeem00@gmail.com>
2022-12-30 15:55:15 +0500

    Improve readability and maintainability of usePhotoGallery function

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -1,75 +1,89 @@
 import { onMounted, reactive, toRefs } from 'vue'
 
 interface Props {
-  lgCols: number
-  mdCols: number
+  columnsOnLgScreens: number
+  columnsOnMdScreens: number
   stateSuffix: string
 }
 
 const usePhotoGallery = (data: unknown[], props: Props) => {
-  const mdScreen = 640
-  const xlScreen = 1280
+  const mdScreenWidth = 640
+  const xlScreenWidth = 1280
+  const maxColumns =
+    props.columnsOnLgScreens > props.columnsOnMdScreens
+      ? props.columnsOnLgScreens
+      : props.columnsOnMdScreens
+  const columns = {}
 
-  const cols = {}
-  for (let i = 1; i <= props.lgCols; i++) {
-    cols[`${props.stateSuffix}${i}`] = []
+  for (let i = 1; i <= maxColumns; i++) {
+    columns[`${props.stateSuffix}${i}`] = []
   }
 
   const state = reactive({
     data: data,
     loading: true,
-    ...cols,
+    ...columns,
   })
 
   const displayLgScreenData = () => {
-    for (let i = 0; i <= props.lgCols; i++) {
-      state[`${props.stateSuffix}${i + 1}`] = state.data
-        .map((data: unknown, index: number) => (index % props.lgCols === i ? data : null))
-        .filter(Boolean)
+    for (let i = 0; i <= props.columnsOnLgScreens; i++) {
+      setTimeout(() => {
+        state[`${props.stateSuffix}${i + 1}`] = state.data
+          .map((data: unknown, index: number) =>
+            index % props.columnsOnLgScreens === i ? data : null
+          )
+          .filter(Boolean)
+      }, 0)
     }
   }
 
   const displayMdScreenData = () => {
-    for (let i = 0; i <= props.mdCols; i++) {
-      state[`${props.stateSuffix}${i + 1}`] = state.data
-        .map((data: unknown, index: number) => (index % props.mdCols === i ? data : null))
-        .filter(Boolean)
+    for (let i = 0; i <= props.columnsOnMdScreens; i++) {
+      setTimeout(() => {
+        state[`${props.stateSuffix}${i + 1}`] = state.data
+          .map((data: unknown, index: number) =>
+            index % props.columnsOnMdScreens === i ? data : null
+          )
+          .filter(Boolean)
+      }, 0)
     }
 
-    for (let i = props.mdCols + 1; i <= props.lgCols; i++) {
-      state[`${props.stateSuffix}${i}`] = []
+    if (props.columnsOnLgScreens > 1) {
+      for (let i = props.columnsOnMdScreens + 1; i <= props.columnsOnLgScreens; i++) {
+        state[`${props.stateSuffix}${i}`] = []
+      }
     }
   }
 
   const displaySmScreenData = () => {
     state[`${props.stateSuffix}${1}`] = state.data
-    for (let i = 2; i <= props.lgCols; i++) {
+    for (let i = 2; i <= props.columnsOnLgScreens; i++) {
       state[`${props.stateSuffix}${i}`] = []
     }
+  }
+
+  const displayData = () => {
+    if (window.innerWidth >= xlScreenWidth && props.columnsOnLgScreens > 1) {
+      displayLgScreenData()
+      return
+    }
+    if (window.innerWidth >= mdScreenWidth && props.columnsOnMdScreens > 1) {
+      displayMdScreenData()
+      return
+    }
+    displaySmScreenData()
   }
 
   const setData = (data: unknown[]) => {
     state.loading = false
     state.data = data
-    if (window.innerWidth >= xlScreen) {
-      displayLgScreenData()
-    } else if (window.innerWidth >= mdScreen) {
-      displayMdScreenData()
-    } else {
-      displaySmScreenData()
-    }
+    displayData()
   }
 
   onMounted(() => {
-    console.log('mounted', state)
+    displayData()
     window.addEventListener('resize', () => {
-      if (window.innerWidth >= xlScreen) {
-        displayLgScreenData()
-      } else if (window.innerWidth >= mdScreen) {
-        displayMdScreenData()
-      } else {
-        displaySmScreenData()
-      }
+      displayData()
     })
   })
 
