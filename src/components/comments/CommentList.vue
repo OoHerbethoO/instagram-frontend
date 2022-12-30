9036bb57685ed77ac03e4d6b6cb14a8f9b1a14c4
safdar-azeem <safdarazeem00@gmail.com>
2022-12-31 00:53:14 +0500

    Add Comment List

--- src/components/comments/CommentList.vue
+++ src/components/comments/CommentList.vue
@@ -1,9 +1,44 @@
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { useGetCommentsQuery, useMeQuery } from '@/types/graphql.types'
+import type { IPost } from '@/types/graphql.types'
+import { defineComponent, watch } from 'vue'
 import CommentItem from './CommentItem.vue'
+
 export default defineComponent({
   name: 'CommentList',
   components: { CommentItem },
+  props: {
+    post: {
+      type: Object as () => IPost,
+      required: true,
+    },
+  },
+  setup(props) {
+    const {
+      result: commentsData,
+      loading: commentsLoading,
+      error: commentsError,
+      refetch: commentsRefetch,
+    } = useGetCommentsQuery({
+      comments: props.post.comments,
+    })
+
+    const { result: meData, loading: meLoading, error: meError } = useMeQuery()
+
+    watch(
+      () => props.post.comments,
+      () => {
+        commentsRefetch({ comments: props.post.comments })
+      }
+    )
+
+    return {
+      commentsData,
+      commentsLoading,
+      commentsError,
+      meData,
+    }
+  },
 })
 </script>
 
@@ -11,12 +46,11 @@ export default defineComponent({
   <div class="md:max-h pb-5 overflow-auto">
     <header class="h4 font-medium mb-3 pb-2 sticky top-0 bg-white w-full">Comments</header>
     <section class="flex flex-col items-start gap-6">
-      <CommentItem />
-      <CommentItem />
-      <CommentItem />
-      <CommentItem />
-      <CommentItem />
-      <CommentItem />
+      <CommentItem
+        v-for="comment in commentsData?.getComments"
+        :key="comment._id"
+        :comment="comment"
+        :me="meData?.me" />
     </section>
   </div>
 </template>
