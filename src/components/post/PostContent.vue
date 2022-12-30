b0d37fb3ceec256a500f2dbf0cfd1768af6e675e
safdar-azeem <safdarazeem00@gmail.com>
2022-12-30 18:21:16 +0500

    Add PostCardContent

---
+++ src/components/post/PostContent.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PostContent',
+  props: {
+    content: {
+      type: String,
+      required: true,
+    },
+    trimText: {
+      type: Boolean,
+      default: true,
+    },
+  },
+})
+</script>
+
+<template>
+  <p
+    class="card-title"
+    :class="{ 'trim-text': trimText }"
+    v-if="content">
+    {{ content }}
+  </p>
+</template>
