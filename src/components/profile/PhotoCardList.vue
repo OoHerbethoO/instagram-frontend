22a94290d8432eb761dd595dd148c32cc263b4b1
safdar-azeem <safdarazeem00@gmail.com>
2022-12-30 15:54:20 +0500

    Add usePhotoGallery to render data in grid

--- src/components/profile/PhotoCardList.vue
+++ src/components/profile/PhotoCardList.vue
@@ -1,9 +1,9 @@
 <script lang="ts">
 import type { IPost } from '@/types/graphql.types'
-import { defineComponent, reactive, toRefs } from 'vue'
+import { defineComponent, reactive, toRefs, watch } from 'vue'
 import PhotoCard from './PhotoCard.vue'
 import PostModal from '../post/PostModal.vue'
-
+import usePhotoGallery from '@/hooks/usePhotoGallery'
 export default defineComponent({
   name: 'PhotoCardList',
   components: { PhotoCard, PostModal },
@@ -17,33 +17,69 @@ export default defineComponent({
       required: true,
     },
   },
-  setup() {
+  setup(props) {
+    const { column1, column2, setData } = usePhotoGallery(props.posts, {
+      columnsOnLgScreens: 0,
+      columnsOnMdScreens: 2,
+      stateSuffix: 'column',
+    })
+
     const state = reactive({
       isModalOpen: false,
       selectedPost: null as IPost | null,
     })
+
+    watch(
+      () => props.posts,
+      () => {
+        if (!props.posts) {
+          state.selectedPost = null
+          state.isModalOpen = false
+          return
+        }
+        setData(props.posts)
+        state.selectedPost =
+          props.posts.find((post) => post._id === state.selectedPost?._id) || null
+      }
+    )
 
     const handlePostClick = (post: IPost) => {
       state.selectedPost = post
       state.isModalOpen = true
     }
 
-    return { ...toRefs(state), handlePostClick }
+    return {
+      ...toRefs(state),
+      handlePostClick,
+      column1: column1 as IPost,
+      column2: column2 as IPost,
+    }
   },
 })
 </script>
 
 <template>
-  <main class="grid sm:grid-cols-2 gap-10 flex-1">
-    <PhotoCard
-      v-for="post in posts"
-      :key="post?._id"
-      @click="handlePostClick(post)"
-      :post="post" />
-    <PostModal
-      :isModalOpen="isModalOpen"
-      @close="isModalOpen = false"
-      :post="selectedPost"
-      @open="isModalOpen = true" />
+  <main class="grid sm:grid-cols-2 gap-6">
+    <div>
+      <PhotoCard
+        v-for="post in column1"
+        :key="post?._id"
+        class="mb-8 md:mb-6"
+        @click="handlePostClick(post)"
+        :post="post" />
+    </div>
+    <div>
+      <PhotoCard
+        v-for="post in column2"
+        :key="post?._id"
+        class="mb-8 md:mb-6"
+        @click="handlePostClick(post)"
+        :post="post" />
+    </div>
   </main>
+  <PostModal
+    :isModalOpen="isModalOpen"
+    @close="isModalOpen = false"
+    :post="selectedPost"
+    @open="isModalOpen = true" />
 </template>
