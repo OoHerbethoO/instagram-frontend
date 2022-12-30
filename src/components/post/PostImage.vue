05295eebcd5a9114f3d47bc9154f10e9e6b6951b
safdar-azeem <safdarazeem00@gmail.com>
2022-12-30 18:21:23 +0500

    Add PostImage

---
+++ src/components/post/PostImage.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PostImage',
+  props: {
+    photo: {
+      type: String,
+      required: true,
+    },
+  },
+})
+</script>
+
+<template>
+  <figure>
+    <img
+      :src="photo || ''"
+      class="card-image"
+      alt="" />
+  </figure>
+</template>
