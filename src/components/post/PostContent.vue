540fc71a3d36ae046039d439a78e05c65f11fc43
safdar-azeem <safdarazeem00@gmail.com>
2023-01-03 17:40:47 +0500

    Update

--- src/components/post/PostContent.vue
+++ src/components/post/PostContent.vue
@@ -1,26 +1,46 @@
 <script lang="ts">
+import type { IPost } from '@/types/graphql.types'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'PostContent',
+  emits: ['openModal'],
   props: {
-    content: {
-      type: String,
+    post: {
+      type: Object as () => IPost,
       required: true,
     },
     trimText: {
       type: Boolean,
       default: true,
     },
+    isModalOpen: {
+      type: Boolean,
+      default: false,
+    },
   },
 })
 </script>
 
 <template>
-  <p
-    class="card-title"
-    :class="{ 'trim-text': trimText }"
-    v-if="content">
-    {{ content }}
-  </p>
+  <div
+    class="gray"
+    :class="{ 'card-content': post.photo === null }">
+    <div class="relative">
+      <span
+        class="card-title"
+        :class="{
+          'trim-text': trimText,
+        }"
+        v-if="post.content">
+        {{ post.content }}
+      </span>
+      <span
+        v-if="!isModalOpen && post.content.length > 400"
+        @click="$emit('openModal')"
+        class="absolute bottom-0 right-0 bg-[#e8e8e8] pl-2 font-bold cursor-pointer text-gray-800">
+        ...Read more
+      </span>
+    </div>
+  </div>
 </template>
