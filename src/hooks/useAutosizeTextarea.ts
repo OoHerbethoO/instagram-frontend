39b59b2263b48a16cdce3c1738a591f9db3ba793
safdar-azeem <safdarazeem00@gmail.com>
2023-01-03 17:44:08 +0500

    Add useAutosizeTextarea Hook

---
+++ src/hooks/useAutosizeTextarea.ts
@@ -0,0 +1,35 @@
+import { ref, onMounted, watch } from 'vue'
+
+export function useAutosizeTextarea(minRows = 1) {
+  const textareaRef = ref(null)
+  console.log('textarea.scrollHeight')
+
+  onMounted(() => {
+    const textarea = textareaRef.value
+    if (textarea) {
+      textarea.style.height = 'auto'
+      textarea.style.height = `${textarea.scrollHeight}px`
+    }
+  })
+
+  function updateSize() {
+    if (textareaRef.value) {
+      textareaRef.value.style.height = 'auto'
+      textareaRef.value.style.height = textareaRef.value.scrollHeight + 'px'
+    }
+  }
+
+  watch(
+    textareaRef,
+    (textarea) => {
+      if (textarea) {
+        console.log(textarea.scrollHeight)
+        textarea.style.height = 'auto'
+        textarea.style.height = `${textarea.scrollHeight}px`
+      }
+    },
+    { immediate: true }
+  )
+
+  return { textareaRef, updateSize }
+}
