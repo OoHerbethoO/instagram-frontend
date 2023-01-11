e3dead7b20b06ac38a2109e8ce97ffcdad1cb5a4
safdar-azeem <safdarazeem00@gmail.com>
2023-01-11 19:44:21 +0500

    Add PostCardSkeleton

--- src/components/skeletons/PostSkeleton.vue
+++ src/components/skeletons/PostSkeleton.vue
@@ -3,29 +3,29 @@ import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'PostCardSkeleton',
+  props: {
+    variant: {
+      type: String as () => 'card' | 'photo',
+      default: 'card',
+    },
+  },
 })
 </script>
 
 <template>
   <article class="post-card">
-    <header class="card-header">
+    <header
+      class="card-header"
+      v-if="variant === 'card'">
       <div class="flex items-center gap-3">
         <div class="avatar skeleton"></div>
         <h1 class="h-6 w-32 rounded skeleton"></h1>
       </div>
-      <div class="avatar md skeleton"></div>
     </header>
-    <div>
-      <h1 class="h-4 w-full rounded skeleton mb-2"></h1>
-      <h1 class="h-4 w-full rounded skeleton"></h1>
+    <div v-if="variant === 'card'">
+      <h1 class="h-3 w-full rounded skeleton mb-2"></h1>
+      <h1 class="h-3 w-full rounded skeleton"></h1>
     </div>
     <div class="card-image skeleton h-64"></div>
-    <footer class="card-footer">
-      <div class="flex items-center gap-5">
-        <div class="avatar skeleton md"></div>
-        <div class="avatar skeleton md"></div>
-      </div>
-      <div class="avatar skeleton md"></div>
-    </footer>
   </article>
 </template>
