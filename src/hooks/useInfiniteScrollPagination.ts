ad1020edb05818b60757784505d285d1bf972a67
safdar-azeem <safdarazeem00@gmail.com>
2023-01-11 19:45:31 +0500

    Add useInfiniteScrollPagination hook

---
+++ src/hooks/useInfiniteScrollPagination.ts
@@ -0,0 +1,18 @@
+import { ref, computed } from 'vue'
+
+export function useInfiniteScrollPagination() {
+  const page = ref(1)
+  const pageSize = ref(10)
+  const skip = computed(() => (page.value - 1) * pageSize.value)
+  const limit = computed(() => pageSize.value)
+
+  const loadMore = async () => {
+    page.value++
+  }
+
+  return {
+    skip,
+    limit,
+    loadMore,
+  }
+}
