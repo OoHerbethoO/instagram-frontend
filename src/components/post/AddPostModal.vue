ca3128fc04591eeb14c38279408e417d3bafbe41
safdar-azeem <safdarazeem00@gmail.com>
2023-01-11 20:24:51 +0500

    Add AddPostModal

---
+++ src/components/post/AddPostModal.vue
@@ -0,0 +1,141 @@
+<script lang="ts">
+import { useAutosizeTextarea } from '@/hooks/useAutosizeTextarea'
+import useImage from '@/hooks/useImage'
+import usePhotoUpload from '@/hooks/usePhotoUpload'
+import { useCreatePostMutation, useMeQuery } from '@/types/graphql.types'
+import { defineComponent, reactive, toRefs } from 'vue'
+import { useToast } from 'vue-toastification'
+import Avatar from '../reusable/Avatar.vue'
+import Button from '../reusable/Button.vue'
+import Modal from '../reusable/Modal.vue'
+import UploadBtn from '../reusable/UploadBtn.vue'
+
+export default defineComponent({
+  name: 'AddPostModal',
+  components: { Button, Avatar, Modal, UploadBtn },
+  setup($props, { emit: $emit }) {
+    const toast = useToast()
+    const { result: me } = useMeQuery()
+    const { textareaRef, updateSize } = useAutosizeTextarea()
+    const { handleUploadPhoto, uploadPhotoLoading } = usePhotoUpload()
+    const {
+      mutate: uploadPost,
+      onError: onCreatePostError,
+      loading: postLoading,
+    } = useCreatePostMutation({
+      refetchQueries: ['GetAllPosts', 'Me', 'GetPostsByUser'],
+    })
+    const { image, readAbleImage, handleImage, cancelImage } = useImage()
+
+    const state = reactive({
+      text: '',
+      isModalOpen: false,
+      maxTextLength: 500,
+    })
+    const handleText = (e: any) => {
+      state.text = e.target.value
+      state.maxTextLength = 500 - state.text.length
+      updateSize()
+    }
+
+    const handleCreatePost = async () => {
+      const photo = await handleUploadPhoto(image.value)
+      const postResult: any = await uploadPost({
+        content: state.text,
+        photo,
+      })
+      if (postResult.data?.createPost) {
+        toast.success('Post created successfully')
+        state.text = ''
+        cancelImage()
+        state.isModalOpen = false
+      }
+    }
+
+    onCreatePostError((error) => toast.error(error.message))
+
+    return {
+      ...toRefs(state),
+      image,
+      readAbleImage,
+      postLoading,
+      uploadPhotoLoading,
+      me,
+      handleImage,
+      cancelImage,
+      handleText,
+      handleCreatePost,
+      textareaRef,
+    }
+  },
+})
+</script>
+
+<template>
+  <Modal
+    title="Create Post"
+    @close="isModalOpen = false"
+    @open="isModalOpen = true"
+    :is-open="isModalOpen"
+    :modalHeight="'h-max'"
+    class="relative"
+    modalFooterClass="block">
+    <template v-slot:trigger>
+      <Button
+        text="Add Post"
+        @click="isModalOpen = true"
+        icon="ion:add-circle" />
+    </template>
+    <template v-slot:modal-body>
+      <main class="flex items-start gap-x-2">
+        <Avatar :src="me?.me?.avatar || ''" />
+        <div class="flex-1">
+          <textarea
+            ref="textareaRef"
+            class="w-full text-area pt-2 theme"
+            placeholder="What’s Happening ?"
+            v-model="text"
+            @input="handleText" />
+          <p
+            class="text-right ml-auto -text-fs-2 text-gray-500 mb-1 h-8 flex items-center justify-center rounded-full w-8"
+            :class="{
+              'text-yellow-600': maxTextLength < 15 && maxTextLength >= 0,
+              'text-danger border-danger': maxTextLength < 0,
+            }">
+            {{ maxTextLength }}
+          </p>
+          <div
+            class="relative"
+            v-if="readAbleImage">
+            <img
+              :src="readAbleImage"
+              class="w-full rounded-lg object-cover"
+              alt="" />
+            <Button
+              icon="ion:close"
+              size="sm"
+              button-class="absolute top-3 right-3 bg-[#19191c6a] "
+              radius="rounded-full"
+              @click="cancelImage" />
+          </div>
+        </div>
+      </main>
+    </template>
+    <template v-slot:modal-footer>
+      <footer class="flex gap-x-2 justify-end items-center">
+        <UploadBtn
+          buttonClass="border-none md"
+          icon="ph:image-square-fill"
+          @handleImage="handleImage" />
+        <Button
+          text="Post"
+          :disabled="(!readAbleImage && !text) || maxTextLength < 0"
+          :isLoading="postLoading || uploadPhotoLoading"
+          button-class="px-7"
+          type="submit"
+          size="md"
+          @click="handleCreatePost" />
+      </footer>
+    </template>
+  </Modal>
+</template>
