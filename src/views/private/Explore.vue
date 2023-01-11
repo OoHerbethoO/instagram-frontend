23c8dc90e807480ef86192dda30872bb5484075d
safdar-azeem <safdarazeem00@gmail.com>
2023-01-11 19:46:45 +0500

    Add Explore Page

---
+++ src/views/private/Explore.vue
@@ -0,0 +1,59 @@
+<script lang="ts">
+import { useExplorePostsLazyQuery } from '@/types/graphql.types'
+import { defineComponent, onMounted, onUnmounted } from 'vue'
+import PostList from '@/components/post/PostList.vue'
+import { useInfiniteScrollPagination } from '@/hooks/useInfiniteScrollPagination'
+
+export default defineComponent({
+  name: 'Explore',
+  components: { PostList },
+  setup() {
+    const { skip, limit, loadMore } = useInfiniteScrollPagination()
+
+    const {
+      load: loadExplorePosts,
+      result: explorePostsData,
+      loading: explorePostsLoading,
+      error: explorePostsError,
+    } = useExplorePostsLazyQuery({
+      skip: skip.value,
+      limit: limit.value,
+    })
+
+    const handleScroll = () => {
+      if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+        loadMore()
+        loadExplorePosts()
+      }
+    }
+
+    onMounted(() => {
+      loadExplorePosts()
+      window.addEventListener('scroll', handleScroll)
+    })
+
+    onUnmounted(() => {
+      window.removeEventListener('scroll', handleScroll)
+    })
+
+    return {
+      explorePostsData,
+      explorePostsLoading,
+      explorePostsError,
+    }
+  },
+})
+</script>
+
+<template>
+  <div>
+    <PostList
+      :posts="explorePostsData?.explorePosts"
+      :loading="explorePostsLoading"
+      :columnsOnLgScreens="3"
+      :columnsOnMdScreens="3"
+      :columnsOnSmScreens="2"
+      :isPostForProfile="true"
+      v-if="explorePostsData?.explorePosts" />
+  </div>
+</template>
