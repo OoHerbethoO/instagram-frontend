47ca9acb003f52ecbb440062ba099087e51894bb
safdar-azeem <safdarazeem00@gmail.com>
2023-01-11 19:46:59 +0500

    Update Root

--- src/views/Root.vue
+++ src/views/Root.vue
@@ -1,8 +1,10 @@
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import PageSidebar from '@/components/reusable/PageSidebar.vue'
 import PageHeader from '@/components/reusable/PageHeader.vue'
 import { useMeQuery } from '@/types/graphql.types'
+import type { OnClickOutsideHandler } from '@vueuse/core'
+import { onClickOutside } from '@vueuse/core'
 
 export default defineComponent({
   name: 'Root',
@@ -12,25 +14,46 @@ export default defineComponent({
   },
 
   setup() {
+    const sidebarRef = ref(null)
+    const isSidebarOpen = ref(false)
+
+    const sidebarHandler: OnClickOutsideHandler = (event) => (isSidebarOpen.value = false)
+    onClickOutside(sidebarRef, (event) => (isSidebarOpen.value = false))
+
     const { result, loading, error } = useMeQuery()
     return {
       result,
       loading,
       error,
+      isSidebarOpen,
+      sidebarRef,
+      sidebarHandler,
     }
+  },
+  computed: {
+    routePath() {
+      return this.$route.path
+    },
   },
 })
 </script>
 
 <template>
   <div class="app-layout">
-    <aside class="app-sidebar">
-      <PageSidebar />
+    <aside
+      class="app-sidebar"
+      ref="sidebarRef"
+      v-on-click-outside.bubble="sidebarHandler"
+      :class="{
+        'hidden lg:flex': !isSidebarOpen,
+        flex: isSidebarOpen,
+      }">
+      <PageSidebar @closeSidebar="isSidebarOpen = false" />
     </aside>
     <main class="app-main w-full">
-      <PageHeader />
+      <PageHeader @openSidebar="isSidebarOpen = !isSidebarOpen" />
       <section class="py-6">
-        <router-view />
+        <router-view :key="routePath" />
       </section>
     </main>
   </div>
