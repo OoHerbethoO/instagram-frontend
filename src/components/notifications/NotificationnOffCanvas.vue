cad8e8bd0351dc47131f0bfc0ab3566c0cc66dbf
safdar-azeem <safdarazeem00@gmail.com>
2023-01-12 04:17:04 +0500

    Add isThereAnyUnSeenNotifications variable to refetch notification if true

--- src/components/notifications/NotificationnOffCanvas.vue
+++ src/components/notifications/NotificationnOffCanvas.vue
@@ -1,9 +1,13 @@
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, watch } from 'vue'
 import NotificationItem from './NotificationItem.vue'
 import Offcanvas from '../reusable/OffCanvas.vue'
 import Button from '../reusable/Button.vue'
-import { useGetNotificationsQuery } from '@/types/graphql.types'
+import {
+  useCountUnSeenNotificationsQuery,
+  useGetNotificationsQuery,
+  useMarkAllNotificationsSeenMutation,
+} from '@/types/graphql.types'
 
 export default defineComponent({
   name: 'NotificationOffCanvas',
@@ -13,19 +17,71 @@ export default defineComponent({
     Button,
   },
   setup() {
-    const { result: notifications, loading, error } = useGetNotificationsQuery()
-    return { notifications, loading, error }
+    const isOffCanvasOpen = ref(false)
+    const isThereAnyUnSeenNotifications = ref(false)
+    const { result: notifications, loading, error, refetch } = useGetNotificationsQuery()
+    const { result: countUnSeenNotificationsResult, loading: countUnSeenNotificationsLoading } =
+      useCountUnSeenNotificationsQuery()
+
+    const { mutate: markAllNotificationsSeen } = useMarkAllNotificationsSeenMutation({
+      updateQueries: {
+        CountUnSeenNotifications: () => {
+          return {
+            countUnSeenNotifications: 0,
+          }
+        },
+      },
+    })
+
+    watch(isOffCanvasOpen, () => {
+      if (
+        isOffCanvasOpen.value &&
+        countUnSeenNotificationsResult?.value.countUnSeenNotifications > 0
+      ) {
+        markAllNotificationsSeen()
+        isThereAnyUnSeenNotifications.value = true
+      }
+    })
+
+    const onClose = () => {
+      if (isThereAnyUnSeenNotifications) refetch()
+      isOffCanvasOpen.value = false
+      isThereAnyUnSeenNotifications.value = false
+    }
+
+    return {
+      notifications,
+      loading,
+      error,
+      countUnSeenNotificationsResult,
+      countUnSeenNotificationsLoading,
+      isOffCanvasOpen,
+      onClose,
+    }
   },
 })
 </script>
 
 <template>
-  <Offcanvas title="Notifications">
+  <Offcanvas
+    title="Notifications"
+    @open="isOffCanvasOpen = true"
+    @close="onClose">
     <template v-slot:trigger>
-      <Button
-        icon="fa-solid:bell"
-        radius="rounded-full"
-        variant="transparent" />
+      <div class="relative">
+        <Button
+          icon="fa-solid:bell"
+          radius="rounded-full"
+          variant="transparent" />
+        <div
+          v-if="
+            !countUnSeenNotificationsLoading &&
+            countUnSeenNotificationsResult?.countUnSeenNotifications > 0
+          "
+          class="absolute top-0 -right-1 bg-danger -text-fs-3 font-bold h-5 w-5 rounded-full grid place-items-center text-white">
+          {{ countUnSeenNotificationsResult?.countUnSeenNotifications }}
+        </div>
+      </div>
     </template>
     <template v-slot:body>
       <p v-if="loading">Loading...</p>
