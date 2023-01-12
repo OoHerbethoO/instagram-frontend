0f726c5a17cf57cd20694fd10f528a257eaea3ea
safdar-azeem <safdarazeem00@gmail.com>
2023-01-13 04:00:40 +0500

    used useImage hook for initial image loading

--- src/components/post/PostImage.vue
+++ src/components/post/PostImage.vue
@@ -1,5 +1,6 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { useImage } from '@vueuse/core'
 
 export default defineComponent({
   name: 'PostImage',
@@ -9,11 +10,20 @@ export default defineComponent({
       required: true,
     },
   },
+  setup(props) {
+    const { isLoading } = useImage({ src: props.photo })
+    return { isImageLoading: isLoading }
+  },
 })
 </script>
 
 <template>
-  <figure class="bg-gray-300 rounded-md">
+  <div
+    v-if="isImageLoading"
+    class="bg-gray-200 h-[350px] w-full rounded-md skeleton"></div>
+  <figure
+    class="bg-gray-200 rounded-md"
+    v-else>
     <img
       :src="photo || ''"
       class="card-image"
