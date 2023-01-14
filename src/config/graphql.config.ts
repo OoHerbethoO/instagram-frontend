73de7a1d41930a173c577f3eb4056014cb851aa6
safdar-azeem <safdarazeem00@gmail.com>
2023-01-14 20:48:38 +0500

    config websocket connection

--- src/config/graphql.config.ts
+++ src/config/graphql.config.ts
@@ -1,8 +1,37 @@
 import Cookies from '@/utils/cookies'
-import { ApolloClient, createHttpLink, InMemoryCache, ApolloLink } from '@apollo/client/core'
+import { ApolloClient, ApolloLink, InMemoryCache, split } from '@apollo/client/core'
 import { setContext } from '@apollo/client/link/context'
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions'
+import { getMainDefinition } from '@apollo/client/utilities'
 import { createUploadLink } from 'apollo-upload-client'
+import { createClient } from 'graphql-ws'
 const env = import.meta.env as any
+
+const httpLink: any = createUploadLink({
+  uri: env.VITE_APP_API_URL,
+  headers: { 'Apollo-Require-Preflight': 'true' },
+})
+
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: env.VITE_APP_WS_API_URL,
+    connectionParams: async () => {
+      const token = Cookies.getToken()
+      return {
+        Authorization: token,
+      }
+    },
+  })
+)
+
+const link = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query)
+    return definition.kind === 'OperationDefinition' && definition.operation === 'subscription'
+  },
+  wsLink,
+  httpLink
+)
 
 const authLink: any = setContext((_, { headers }) => {
   const authHeader: any = {}
@@ -18,13 +47,8 @@ const authLink: any = setContext((_, { headers }) => {
   }
 })
 
-const httpLink: any = createUploadLink({
-  uri: env.VITE_APP_API_URL,
-  headers: { 'Apollo-Require-Preflight': 'true' },
-})
-
 const apolloClient = new ApolloClient({
-  link: ApolloLink.from([authLink, httpLink]),
+  link: ApolloLink.from([authLink, link]),
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
