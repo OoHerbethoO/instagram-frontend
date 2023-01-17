c423efd29668b925a57251576b0da2a44fc59db4
safdar-azeem <safdarazeem00@gmail.com>
2023-01-17 19:33:24 +0500

    Fix ml

--- src/components/reusable/PageHeader.vue
+++ src/components/reusable/PageHeader.vue
@@ -43,7 +43,7 @@ export default defineComponent({
           radius="rounded-full"
           iconClass="text-fs-2"
           :class="{
-            'sm:ml-[-5px]': showInput,
+            'ml-[-5px]': showInput,
           }"
           @click="showInput = !showInput"
           buttonClass="ml-1 mr-0" />
